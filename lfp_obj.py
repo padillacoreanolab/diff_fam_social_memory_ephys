ad8adf02ce37a0913b3f1820ed27604856b1aef7
Chaitra Peddireddy <cpeddireddy@ufl.edu>
2023-11-19 17:58:32 -0500

    Made New DF from rce_tone_times

--- lfp_obj.py
+++ lfp_obj.py
@@ -1,31 +1,32 @@
 import pandas as pd
+
+#assuming condition col = win/ loss --> unique --> all event types
+#assuming subject_info is the subject attributed in the individual df
+
 class LFPrecordingCollection:
     def __init__(self, path_to_excel):
         self.path = path_to_excel
 
+        #type_event: dictionary "times": [start, stop], "subject": X, "title_rec": Y
+
         def parse_all_trials(self, path):
+
             data = pd.read_excel(path)
             # assuming the "time" column is the start time
             # adding const to time column to get end time
             self.data = pd.read_excel(self.path)
-            #iterate through unique video_files and make a new dataframe for each
-            tempDF = pd.DataFrame()
-            #add unique video files as column in dataframe
-            tempDF["video_file"] = self.data["video_file"].unique()
-            #iterate through original data to add event, start, end to new dataframe
-            for index, row in self.data.iterrows():
-                #add event
-                tempDF["event"] = row["event"]
-                #add start time
-                tempDF["start_time"] = row["time_stamp"]
-                #add end time
-                tempDF["end_time"] = row["end_time"]
+            #drop all the NaN values
+            self.data = self.data.dropna()
+            #create new dataframe
+            newDF = pd.DataFrame()
+            newDF = self.data[["video_file", "condition", "time_stamp_index", "subject_info"]].copy()
 
-            # convert time to int
-            self.data["time_stamp"] = self.data["time_stamp"].astype(int)
-            # adding end time column
-            # TODO: ask albert if end time exists
-            self.data["end_time"] = self.data["time_stamp"] + 10000
+            newDF["end_time"] = newDF["time_stamp_index"] + 10000
 
-test = pd.read_excel("rce_tone_timestamp.xlsx")
-print(test["time_stamp_index"])
+            #convert to dict
+            newDF = newDF.to_dict("records")
+            #print keys
+            print(newDF[0].keys())
+        parse_all_trials(self, self.path)
+
+testData = LFPrecordingCollection("rce_tone_timestamp.xlsx")
