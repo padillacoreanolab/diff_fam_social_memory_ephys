179a8ddf696df7f457ec06342049c25400d3a347
Chaitra Peddireddy <cpeddireddy@ufl.edu>
2024-01-28 18:05:02 -0500

    Update bash script to use wildcard

--- pipeline/convert_to_mp4.sh
+++ pipeline/convert_to_mp4.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-#/scratch/back_up/reward_competition_extention
+
+# This script converts .h264 video files to .mp4 and then re-encodes them.
+# It also creates a log file with the names of the input and output files.
+
+# Set the project directory
 project_dir=$1
-#${project_dir}
-experiment_dir=$2
-
 echo "Project directory: ${project_dir}"
-echo "Experiment directory: ${experiment_dir}"
 
-cd ${experiment_dir}
+# Change the current directory to the project directory
+cd ${project_dir}
 
-video_directory=${experiment_dir}/data
+# Set the directories for the input videos and the output videos
+video_directory=${project_dir}/data
 output_directory=${project_dir}/proc/reencoded_videos
 
-# Process function
+# Enable globstar option to use ** for matching files in nested directories
+shopt -s globstar
+
+# Function to convert .h264 video files to .mp4
 convert_h264_to_mp4() {
     input_file=$1
     output_file=$2
 
-    echo "Currently working on in convert: ${input_file}"
+    echo "output file: ${output_file}"
+
+    echo Currently working on: ${input_file}
 
     # Check if output file already exists
     if [ -f "${output_file}" ]; then
@@ -35,12 +42,12 @@ convert_h264_to_mp4() {
     fi
 }
 
-# Process function
+# Function to re-encode .mp4 video files
 reencode_mp4() {
     input_file=$1
     output_file=$2
 
-    echo "Currently working on in reencode_mp4: ${input_file}"
+    echo Currently working on: ${input_file}
 
     # Check if output file already exists
     if [ -f "${output_file}" ]; then
@@ -59,28 +66,34 @@ reencode_mp4() {
 }
 
 
-####################################################################################################################
 
 
-for full_path in ${video_directory}/*/*/*/*.h264; do
+# Loop over all .h264 files in the video directory and its subdirectories
+for full_path in ${video_directory}/**/*.h264; do
+    # Print the name of the file currently being processed
     echo "Currently starting: ${full_path}"
 
+    # Get the directory name, file name, and base name of the file
     dir_name=$(dirname ${full_path})
     file_name=${full_path##*/}
     base_name="${file_name%.h264}"
     recording_name=${base_name%%.*}
     
+    # Create a directory for the output files
     recording_dir=${output_directory}/${recording_name}
     mkdir -p ${recording_dir}
 
-    # Replace this with how you form your output file name
+    # Form the output file name for the converted .mp4 file
     converted_mp4_path=${recording_dir}/${base_name}.original.mp4
+    # Convert the .h264 file to .mp4
     convert_h264_to_mp4 ${full_path} ${converted_mp4_path}
 
-    # Replace this with how you form your output file name
+    # Form the output file name for the re-encoded .mp4 file
     reencoded_mp4_path=${recording_dir}/${base_name}.fixed.mp4
+    # Re-encode the .mp4 file
     reencode_mp4 ${converted_mp4_path} ${reencoded_mp4_path} 
 
 done
 
+# Print a message when all files have been processed
 echo All Done!
--- pipeline/lfp_analysis.py
+++ pipeline/lfp_analysis.py
@@ -1,9 +1,9 @@
 import subprocess
 import os
 
-experiment_dir = "/Volumes/chaitra/test_lfp/data/omission_reward_competition"
-project_dir = "/Volumes/chaitra/test_lfp/data/omission_reward_competition"
+experiment_dir = "/Volumes/chaitra/test_lfp"
+project_dir = "/Volumes/chaitra/test_lfp"
 bash_path = "./convert_to_mp4.sh"
 print(os.getcwd())
 
-subprocess.run([bash_path, experiment_dir, project_dir])
+subprocess.run([bash_path, experiment_dir])
