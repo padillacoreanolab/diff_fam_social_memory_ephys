acc926f3d0a4983d28171775f94347134de77400
Chaitra Peddireddy <cpeddireddy@ufl.edu>
2024-02-07 09:56:59 -0500

    Added trodes functions

--- pipeline/lfp_analysis.py
+++ pipeline/lfp_analysis.py
@@ -1,5 +1,8 @@
+import glob
 import subprocess
 import os
+from collections import defaultdict
+import trodes.read_exported
 def convert_to_mp4(experiment_dir):
     """
     Converts .h264 files to .mp4 files using the bash script convert_to_mp4.sh
@@ -15,11 +18,40 @@ def convert_to_mp4(experiment_dir):
 
 
 experiment_dir = "/Volumes/chaitra/test_lfp"
-convert_to_mp4(experiment_dir)
+#convert_to_mp4(experiment_dir)
 
-#Params for 00 notebook
-input_dir = "/Volumes/chaitra/test_lfp/data/"
+def extract_all_trodes(input_dir, output_dir, tone_din, tone_state):
+    def recursive_dict():
+        return defaultdict(recursive_dict)
+
+    session_to_trodes_data = recursive_dict()
+    session_to_path = {}
+    print(trodes.read_exported.organize_all_trodes_export("/Volumes/chaitra/test_lfp/data/omission_reward_competition/"))
+    """
+    for session in glob.glob(input_dir):
+        print("Processing session: ", session)
+        try:
+            session_basename = os.path.splitext(os.path.basename(session))[0]
+            print("Processing session: ", session_basename)
+            if os.path.isdir(session):
+                print("this session is a directory")
+                print(trodes.read_exported.organize_all_trodes_export(session))
+            session_to_trodes_data[session_basename] = trodes.read_exported.organize_all_trodes_export(session)
+            session_to_path[session_basename] = session
+        except Exception as e:
+            print("Error processing session: ", session_basename)
+            print(e)
+    print(session_to_trodes_data)
+    """
+    return session_to_trodes_data
+
+# Params for 00 notebook
+input_dir = "/Volumes/chaitra/test_lfp/data/omission_reward_competition/2023_06_*/*.rec"
 output_dir = "/Volumes/chaitra/test_lfp/proc/"
+TONE_DIN = "dio_ECU_Din1"
+TONE_STATE = 1
+extract_all_trodes(input_dir, output_dir, TONE_DIN, TONE_STATE)
+
 
 # tone din = dio_ECU_Din1
 
