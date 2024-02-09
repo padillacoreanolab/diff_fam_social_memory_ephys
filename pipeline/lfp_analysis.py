57ab223a03308cd88f0fe9d76f8edf519d907e93
Chaitra Peddireddy <cpeddireddy@ufl.edu>
2024-02-09 10:11:00 -0500

    Add documentation

--- pipeline/lfp_analysis.py
+++ pipeline/lfp_analysis.py
@@ -30,29 +30,36 @@ def convert_to_mp4(experiment_dir):
     Converts .h264 files to .mp4 files using the bash script convert_to_mp4.sh
     convert_to_mp4.sh should exist in the same directory as this script.
     Args:
-        experiment_dir (String): Path to the experiment directory containing .h264 files
-
+        experiment_dir (String): Path to the experiment directory containing subdirectories with .h264 files.
+            For example, if your experiment contains the following subdirectories:
+                /path/to/experiment/trial1
+                /path/to/experiment/trial2
+            Your experiment_dir should be /path/to/experiment.
     Returns:
         None
     """
     bash_path = "./convert_to_mp4.sh"
     subprocess.run([bash_path, experiment_dir])
 
-
 experiment_dir = "/Volumes/chaitra/test_lfp"
 #convert_to_mp4(experiment_dir)
-def extract_all_trodes(input_dir, output_dir, tone_din, tone_state):
+def extract_all_trodes(input_dir):
+    """
+    Args:
+        input_dir (String): Path containing the session directories to process.
+
+    Returns:
+        session_to_trodes_data (defaultdict): A nested dictionary containing the metadata for each session.
+    """
+
     def recursive_dict():
         return defaultdict(recursive_dict)
 
     session_to_trodes_data = recursive_dict()
     session_to_path = {}
 
-    # session directory is the folder that contains a single session's worth of data and metadata (has sub directories)
-    # contains merged.rec file
-
-    # raw files would not be file
-    # print(trodes.read_exported.read_trodes_extracted_data_file("Downloads/20221122_164720_competition_6_1_top_3__base_3_merged.raw_group0.dat"))
+    # This loop will process each session directory using the trodes extract functions and store the metadata in a
+    # nested dictionary.
 
     for session in glob.glob(input_dir):
         try:
@@ -63,36 +70,59 @@ def extract_all_trodes(input_dir, output_dir, tone_din, tone_state):
         except Exception as e:
             print("Error processing session: ", session_basename)
             print(e)
-    print(session_to_trodes_data)
+
+    # print(session_to_trodes_data)
     return session_to_trodes_data, session_to_path
 
 def add_video_timestamps(session_to_trodes_data, directory_path):
-    print("IN ADD VIDEO TIMESTAMPS")
+    """
+    Args:
+        session_to_trodes_data (Nested Dictionary): Generate from extract_all_trodes.
+        directory_path (String): Path containing the session directories to process.
+
+    Returns:
+        session_to_trodes_data (Nested Dictionary): A nested dictionary containing the metadata for each session.
+    """
+
+    # Loops through each session and video_timestamps file and adds the timestamps to the session_to_trodes_data
+    # dictionary. Timestamp array is generated using the read_trodes_extracted_data_file function from the
+    # trodes.read_exported module.
+
     for session in glob.glob(directory_path):
-        print("IN SESSION " + session)
         try:
             session_basename = os.path.splitext(os.path.basename(session))[0]
             print("Current Session: {}".format(session_basename))
-            file_to_video_timestamps = {}
+
             for video_timestamps in glob.glob(os.path.join(session, "*cameraHWSync")):
-                print("IN CAMERA HWSYNC")
                 video_basename = os.path.basename(video_timestamps)
                 print("Current Video Name: {}".format(video_basename))
                 timestamp_array = trodes.read_exported.read_trodes_extracted_data_file(video_timestamps)
+
                 if "video_timestamps" not in session_to_trodes_data[session_basename][session_basename]:
                     session_to_trodes_data[session_basename][session_basename]["video_timestamps"] = defaultdict(dict)
+
                 session_to_trodes_data[session_basename][session_basename]["video_timestamps"][video_basename.split(".")[-3]] = timestamp_array
-                print("Timestamp Array for {}: ".format(video_basename))
-                print(timestamp_array)
+                # print("Timestamp Array for {}: ".format(video_basename))
+                # print(timestamp_array)
+
         except Exception as e:
             print("Error processing session: ", session_basename)
             print(e)
-        #print the timestamp array
 
         return session_to_trodes_data
 
 def create_metadata_df(session_to_trodes, session_to_path):
-    # create a dataframe from the nested dictionary
+    """
+
+    Args:
+        session_to_trodes (nested dictionary): Generated from extract_all_trodes.
+        session_to_path (empty dictionary): {}
+        columns_to_keep (dictionary): Provide a dictionary of the columns to keep in the metadata dataframe.
+
+    Returns:
+        trodes_metadata_df (pandas dataframe): A dataframe containing the metadata for each session.
+    """
+
     trodes_metadata_df = pd.DataFrame.from_dict({(i,j,k,l): session_to_trodes[i][j][k][l]
                             for i in session_to_trodes.keys()
                             for j in session_to_trodes[i].keys()
@@ -100,9 +130,16 @@ def create_metadata_df(session_to_trodes, session_to_path):
                             for l in session_to_trodes[i][j][k].keys()},
                             orient='index')
 
+    # Rename columns during t
+
     trodes_metadata_df = trodes_metadata_df.reset_index()
+    print(type(trodes_metadata_df))
+    print("HERE")
+    print(trodes_metadata_df.columns)
     trodes_metadata_df = trodes_metadata_df.rename(columns={'level_0': 'session_dir', 'level_1': 'recording', 'level_2': 'metadata_dir', 'level_3': 'metadata_file'}, errors="ignore")
     trodes_metadata_df["session_path"] = trodes_metadata_df["session_dir"].map(session_to_path)
+    print("After filter")
+    print(trodes_metadata_df.columns)
     return trodes_metadata_df
 
 def add_subjects_to_metadata(metadata):
@@ -186,48 +223,7 @@ TONE_STATE = 1
 experiment_dir = "/Volumes/chaitra/reward_competition_extension/data"
 #convert_to_mp4(experiment_dir)
 paths = {}
-session_to_trodes_temp, paths= extract_all_trodes(input_dir, output_dir, TONE_DIN, TONE_STATE)
+session_to_trodes_temp, paths= extract_all_trodes(input_dir)
 session_to_trodes_temp = add_video_timestamps(session_to_trodes_temp, input_dir)
 metadata = create_metadata_df(session_to_trodes_temp, paths)
 metadata, state_df, video_df = adjust_first_timestamps(metadata)
-
-
-
-
-# print all values in first 5 rows
-for i in range(5):
-    print(metadata.iloc[i])
-
-
-#creating merged.rec is manual process
-
-# tone din = dio_ECU_Din1
-
-#session to trodes data = session base name is key, value is the metdata
-
-# trodes function on line 16
-# session_to_trodes_data[session_basename] = trodes.read_exported.organize_all_trodes_export(session_path)
-# https://github.com/padillacoreanolab/reward_competition_extention/blob/main/src/trodes/read_exported.py
-# could possibly be combied with trodes function run on video_timestamps
-
-
-# metadata df = entire metadata,
-# METADATA_TO_KEEP = ['raw', 'DIO', 'video_timestamps']
-
-#trodes_state_df = timestamp of when the state of component change
-# lambda function for state df when last item is one, get timestamp
-
-
-"""
-trodes_metadata_df["data"].iloc[0]
-array([(3478533, 0)], dtype=[('time', '<u4'), ('state', 'u1')])
-"""
-
-#recording is usually the key for the merged data frames
-# original file is the name of the metadata file being referenced (in the final DF)
-
-#making timestamp 0 index
-# subtract first timestamp from every other timestamp
-
-#np int 32 for faster computation
-
